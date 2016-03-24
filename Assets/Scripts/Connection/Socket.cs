bf0a79f4e5dff8c6d9f47c3d98ab964733949917
igna92ts <igna_torres92@hotmail.com>
2016-03-24 01:24:08 -0300

    first commit

---
+++ Assets/Scripts/Connection/Socket.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class Socket : MonoBehaviour {
+
+	public UdpClient client;
+	public IPAddress serverIp;
+	public string hostIp;
+	public int hostPort;
+	public IPEndPoint hostEndPoint;
+
+	void Start(){
+		serverIp = IPAddress.Parse(hostIp);
+		hostEndPoint = new IPEndPoint(serverIp,hostPort);
+
+		client = new UdpClient();
+		client.Connect(hostEndPoint);
+		client.Client.Blocking = false;
+	}
+
+	public void SendDgram(string evento,string msg){
+		byte[] dgram = Encoding.UTF8.GetBytes(evento+"_"+msg);
+		client.Send(dgram,dgram.Length);
+		client.BeginReceive(new AsyncCallback(processDgram),client);
+	}
+
+	public void processDgram(IAsyncResult res){
+		try {
+			byte[] recieved = client.EndReceive(res,ref hostEndPoint);
+			Debug.Log(Encoding.UTF8.GetString(recieved));
+		} catch (Exception ex) {
+			throw ex;
+		}
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button (new Rect (10,10,100,40), "Send"))
+		{
+			DynamicObject d = new DynamicObject();
+			SendDgram("JSON",JsonUtility.ToJson(d).ToString());
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/DynamicObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class DynamicObject{
+
+	public int id;
+	public string type;
+	public Vector2 position;
+	public Quaternion rotation;
+	public Vector3 linearVelocity;
+	public float angularVelocity;
+	public string ownedBy = "";
+
+	public void setValues(int id,string type,Vector2 position,Quaternion rotation,Vector3 linearVelocity,float angularVelocity){
+		this.id = id;
+		this.type = type;
+		this.position = position;
+		this.rotation = rotation;
+		this.linearVelocity = linearVelocity;
+		this.angularVelocity = angularVelocity;
+	}
+
+	public void setValues(string type,Vector2 position,Quaternion rotation,Vector3 linearVelocity,float angularVelocity){
+		this.type = type;
+		this.position = position;
+		this.rotation = rotation;
+		this.linearVelocity = linearVelocity;
+		this.angularVelocity = angularVelocity;
+	}
+
+	public void setValues(Vector2 position,Quaternion rotation,Vector3 linearVelocity,float angularVelocity){
+		this.position = position;
+		this.rotation = rotation;
+		this.linearVelocity = linearVelocity;
+		this.angularVelocity = angularVelocity;
+	}
+
+}
